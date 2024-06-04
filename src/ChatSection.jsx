2d263fcbe96650425f2de9dc565ffd23b297fea0
Ritu Raj <rajritu1729@gmail.com>
2024-06-05 00:16:16 +0530

    faltu1

--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -1,21 +1,18 @@
 import "./styles/ChatSection.css";
 
-function ChatSection(){
-    return(
-     
-            
-            <div className="chat-section">
-            <div className="top-bar">
-                <img src="./images/turtle.png" className="turtle"/>
-                <div>Turtle</div>
-                <p>Online</p>
-            </div>   
-                <div>
-                    <input placeholder="Write" type="text" className="chat-input"/>
-                </div>
-            </div>
-
-    )
+function ChatSection() {
+  return (
+    <div className="chat-section">
+      <div className="top-bar">
+        <img src="./images/turtle.png" className="turtle" />
+        <div>Turtle</div>
+        <p>Online</p>
+      </div>
+      <div>
+        <input placeholder="Write" type="text" className="chat-input" />
+      </div>
+    </div>
+  );
 }
 
 export default ChatSection;
