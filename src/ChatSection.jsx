027e426899ab9640027c4a92d7d3ce9ddfef247e
raveena222 <ravenna2202@gmail.com>
2024-06-04 16:16:36 +0530

    change2

--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -2,21 +2,7 @@ import "./styles/ChatSection.css";
 
 function ChatSection(){
     return(
-        <div>
-            <div className="top-bar">
-                <img src="./images/turtle.png" className="turtle"/>
-                <div>Turtle</div>
-                <p>Online</p>
-            </div>    
-            <div className="chat-section">
-                <div>
-
-                </div>
-                <div>
-                    <input placeholder="Write" type="text" className="chat-input"/>
-                </div>
-            </div>
-        </div>
+        
     )
 }
 
