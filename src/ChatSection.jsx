4b639b92f57144a5e007d0d251c87c538fadc093
raveena222 <ravenna2202@gmail.com>
2024-06-05 00:11:30 +0530

    chatdesign

--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -1,24 +1,28 @@
 import "./styles/ChatSection.css";
+import turtle from "./assets/turtle.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane, faPaperclip, faFaceSmile } from "@fortawesome/free-solid-svg-icons";
 
 function ChatSection(){
     return(
-<<<<<<< HEAD
-        
-=======
-     
-            
-            <div className="chat-section">
+        <div className="chat-section">
             <div className="top-bar">
-                <img src="./images/turtle.png" className="turtle"/>
-                <div>Turtle</div>
-                <p>Online</p>
-            </div>   
-                <div>
-                    <input placeholder="Write" type="text" className="chat-input"/>
-                </div>
-            </div>
+                <img src={turtle} className="turtle"/>
+                <span className="userName">Turtle</span>
+            </div> 
+            <div className="allChats">
+                 
+            </div>  
+            <div className="chatinput">
 
->>>>>>> a410746fa52e69c0d8f960d64537574789b5ac7e
+                <FontAwesomeIcon icon={faFaceSmile} size="lg" className="smile"/>
+                <input className="chatInput" placeholder="Write" type="text"/>
+                <FontAwesomeIcon icon={faPaperclip} size="lg" className="clip"/>
+                <FontAwesomeIcon icon={faPaperPlane} size="lg"/>
+                
+            </div>
+        </div>
+
     )
 }
 
