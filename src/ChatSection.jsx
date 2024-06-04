a410746fa52e69c0d8f960d64537574789b5ac7e
Ritu Raj <rajritu1729@gmail.com>
2024-06-04 16:01:13 +0530

    corrected

--- src/ChatSection.jsx
+++ src/ChatSection.jsx
@@ -2,21 +2,19 @@ import "./styles/ChatSection.css";
 
 function ChatSection(){
     return(
-        <div>
+     
+            
+            <div className="chat-section">
             <div className="top-bar">
                 <img src="./images/turtle.png" className="turtle"/>
                 <div>Turtle</div>
                 <p>Online</p>
-            </div>    
-            <div className="chat-section">
-                <div>
-
-                </div>
+            </div>   
                 <div>
                     <input placeholder="Write" type="text" className="chat-input"/>
                 </div>
             </div>
-        </div>
+
     )
 }
 
