66fbdb00c3e5c2b7db9c08ec7efe69846db6e027
Ritu Raj <rajritu1729@gmail.com>
2024-06-17 22:04:40 +0530

    17JUN

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import "./App.css";
-import "./styles/responsive.css";
 import { useState } from "react";
 import Home from "./Home";
 
 function App() {
   const [tog, setTog] = useState(false);
+  const [togs, setTogs] = useState(false);
 
   function handleTog() {
     setTog(!tog);
   }
 
+  function handleTogs() {
+    setTogs(!togs);
+  }
+
   return (
     <>
       <div className="App">
-        <Home handleTog={handleTog} tog={tog} />
+        <Home handleTogs={handleTogs} handleTog={handleTog} tog={tog} togs={togs}/>
       </div>
     </>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,28 @@ import NavDetails from "./NavDetails";
 import Sidebar from "./Sidebar";
 import ChatSection from "./ChatSection";
 import Profile from "./Profile";
+import Settings from "./Settings";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
 function Home({ tog, handleTog }) {
+  
   return (
     <>
-      <NavDetails handleTog={handleTog} />
-      {tog ? <Profile handleTog={handleTog}/> : <Sidebar handleTog={handleTog} />}
-      <ChatSection />
+      <NavDetails  handleTog={handleTog} />
+     
+         
+          <Routes>
+            <Route path="/real-time-chatapp" element={<Sidebar />} />
+            <Route path="/real-time-chatapp/Profile" element={<Profile />} />
+            <Route path="/real-time-chatapp/settings" element={<Settings />} />
+          </Routes>
+        
+        
+       
+          <ChatSection />
+     
+      
     </>
   );
 }
