15e85426baa46a75b7b25d4e0108820702fe3c99
raveena222 <ravenna2202@gmail.com>
2024-06-21 23:47:03 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
 import "./styles/responsive.css";
 import { useEffect } from "react";
-=======
-import { useState } from "react";
->>>>>>> 66fbdb00c3e5c2b7db9c08ec7efe69846db6e027
 import Home from "./Home";
 import LoginPage from "./userAuth/LoginPage.jsx";
 import Notification from "./userAuth/Notification.jsx";
@@ -13,15 +9,10 @@ import {auth} from "./backend/firebase.js";
 // import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-<<<<<<< HEAD
   // const [tog, setTog] = useState(false);
   // function handleTog() {
   //   setTog(!tog);
   // }
-=======
-  const [tog, setTog] = useState(false);
-  const [togs, setTogs] = useState(false);
->>>>>>> 66fbdb00c3e5c2b7db9c08ec7efe69846db6e027
 
   const user= false;
 
@@ -37,17 +28,11 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       {/* <div className="App"> */}
          {user ? (<Home/>)
          :(<LoginPage/>)}
       {/* </div> */}
       <Notification/>
-=======
-      <div className="App">
-        <Home handleTogs={handleTogs} handleTog={handleTog} tog={tog} togs={togs}/>
-      </div>
->>>>>>> 66fbdb00c3e5c2b7db9c08ec7efe69846db6e027
     </>
   );
 }
